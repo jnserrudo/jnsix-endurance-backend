92611c3caa8a81847fc2813a505082287f4eeab0
jnserrudo1 <jnserrudo@ucasal.edu.ar>
2026-07-20 16:44:15 -0300

    fixs

--- src/controllers/stripe.controller.js
+++ src/controllers/stripe.controller.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy');
 const prisma = require('../lib/prisma');
 
 const createCheckoutSession = async (req, res) => {
