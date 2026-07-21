772a4b4fe6dee84ab715bf1e2f2dc52f8c949284
jnserrudo1 <jnserrudo@ucasal.edu.ar>
2026-07-20 23:16:58 -0300

    fix: corregir error 500 del feed por recuento de reactions

--- src/controllers/feed.controller.js
+++ src/controllers/feed.controller.js
@@ -55,7 +55,7 @@ const getFeed = async (req, res) => {
       include: {
         user: { select: { id: true, email: true } },
         activity: { select: { id: true, name: true } },
-        _count: { select: { comments: true, reactions: true } }
+        _count: { select: { comments: true } }
       }
     });
 
